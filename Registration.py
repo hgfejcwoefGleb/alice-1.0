94ffd49e3e1432a97f40a436b94c256bd2c982b5
hgfejcwoefGleb <114211284+hgfejcwoefGleb@users.noreply.github.com>
2025-02-25 22:41:53 +0300

    Add files via upload

--- Registration.py
+++ Registration.py
@@ -1,6 +1,10 @@
 import psycopg2
 
 
+#продумать заполнение таблиц, в соответствии с тем, как они свзяаны
+#писать сразу нормально с обработкой ошибок
+#id должен обновляться автоматически
+#добавить в группу атрибут Date, который бы отражал на какие даты стоят пары
 class Group:
     def __init__(self, name, edu_year, edu_program, faculty, edu_format, edu_level, cur):
         #print(name, edu_year, edu_program, faculty, edu_format, edu_level)
@@ -10,14 +14,14 @@ class Group:
         self.faculty = faculty
         self.edu_format = edu_format
         self.edu_level = edu_level
-        print(vars(self))
         #cur.execute('INSERT INTO "Group" (name, edu_year, edu_program, faculty, edu_format, edu_level) '
-                    #'VALUES (%s, %s, %s, %s, %s, %s);', list(vars(self).values())) #??????????
-        cur.execute("""INSERT INTO "Group" (name, edu_year, edu_program, faculty, edu_format, edu_level) 
-         VALUES (2, 2, 2, 2, 2, 2);""")
+        #'VALUES (%s, %s, %s, %s, %s, %s);', list(vars(self).values())) #??????????
+        #cur.execute("""INSERT INTO "Group" (name, edu_year, edu_program, faculty, edu_format, edu_level)
+        #VALUES (2, 2, 2, 2, 2, 2);""")
+
 
 class Student:
-    def __int__(self, name, surname, father_name, id_group, cur):
+    def __init__(self, name, surname, father_name, id_group, cur):
         self.name = name
         self.surname = surname
         self.father_name = father_name
@@ -25,14 +29,14 @@ class Student:
 
 
 class Lecturer:
-    def __int__(self, name, surname, father_name, cur):
+    def __init__(self, name, surname, father_name, cur):
         self.name = name
         self.surname = surname
         self.father_name = father_name
 
 
 class PersonalLesson:
-    def __int__(self, name, type_l, building, auditorium, id_lecturer, time, week_day, is_upper, id_student, cur):
+    def __init__(self, name, type_l, building, auditorium, id_lecturer, time, week_day, is_upper, id_student, cur):
         self.name = name
         self.type_l = type_l
         self.building = building
@@ -45,13 +49,13 @@ class PersonalLesson:
 
 
 class PersonalLessonStudent:
-    def __int__(self, id_student, id_personal_lesson, cur):
+    def __init__(self, id_student, id_personal_lesson, cur):
         self.id_student = id_student
         self.id_personal_lesson = id_personal_lesson
 
 
 class GroupLesson:
-    def __int__(self, name, type_l, building, auditorium, id_lecturer, time, week_day, is_upper, id_group, cur):
+    def __init__(self, name, type_l, building, auditorium, id_lecturer, time, week_day, is_upper, id_group, cur):
         self.name = name
         self.type_l = type_l
         self.building = building
@@ -64,7 +68,7 @@ class GroupLesson:
 
 
 class GroupLessonGroup:
-    def __int__(self, id_group_lesson, id_group, cur):
+    def __init__(self, id_group_lesson, id_group, cur):
         self.id_group_lesson = id_group_lesson
         self.id_group = id_group
 
@@ -76,36 +80,42 @@ def connect():
         password='1234',
         host='localhost',
         port='5432'
-        )
+    )
     cur = conn.cursor()
-    return cur
+    return conn, cur
+
+
 #подключение к БД происходит один раз в начале регистрации
-def registration_student(user_data, group_data, is_student): #приходит какая-то информация о пользователе
-    cur = connect()
+def registration_user(user_data, group_data, is_student):  #приходит какая-то информация о пользователе
+    """Происходит получение информации о пользователе и группе и данные заносятся в таблицу student или lecturer"""
+    conn, cur = connect()
     if is_student:
-        #print(type(group_data))
-        group = Group(*group_data, cur) #
-        #student = Student(*user_data, cur)
-    #else:
-        #teacher = Lecturer(user_data, cur)
+        group = Group(*group_data, cur)
+        student = Student(*user_data, cur)
+        if not is_group_reg(group):  #сначала заполняется таблица группы, если группы нет
+            cur.execute("insert into group (id, name, edu_year, edu_program, faculty, edu_format, edu_level) "
+                        "values (%s, %s, %s, %s, %s, %s);", (1, group.name, group.edu_year, group.edu_program,
+                                                             group.faculty, group.edu_format, group.edu_level))
+        cur.execute(f"insert into student (id, name, surname, father_name, id_group) values (%s, %s, %s, %s, %s);",
+                    (2, student.name, student.surname, student.father_name, student.id_group))
+    else:
+        lecturer = Lecturer(*user_data, cur)
+        cur.execute(f"insert into lecturer (id, name, surname, father_name) values (%s, %s, %s, %s);",
+                    (1, lecturer.name, lecturer.surname, lecturer.father_name))
+    conn.commit()
     cur.close()
-    #connect.close()
+    conn.close()
 
 
+def is_group_reg(group):
+    """Можно на вход принимать только группу и проверять по name, edu_year в таблице.
+        Если группа есть в таблице возвращает все ее атрибуты, иначе None
+    """
+    conn, cur = connect()
+    cur.execute(f"select * from group where name={group.name} and edu_year={group.edu_year}")
+    res = cur.fetchone()
+    cur.close()
+    conn.close()
+    return res is not None
 
-    """import psycopg2
 
-with psycopg2.connect(
-dbname="selecteldb",
-user="postgres",
-password="password",
-host="localhost",
-port="5432"
-) as conn:
-with conn.cursor() as cur:
-    # Вставка данных
-    cur.execute("INSERT INTO servers (server, cost) VALUES (%s, %s);", ("server1", 100))
-    cur.execute("INSERT INTO servers (server, cost) VALUES (%s, %s);", ("server2", 200))
-    cur.execute("INSERT INTO servers (server, cost) VALUES (%s, %s);", ("server3", 300))
-    conn.commit()"""
-    pass
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import Registration as R
 #name, edu_year, edu_program, faculty, edu_format, edu_level, cur
-R.registration_student([1], ['1', '1', '1', '1', '1', '1'], True)
+R.registration_user(["Олег", "Козлина", "Петрович", "1"], ['1', '1', '1', '1', '1', '1'], True)
