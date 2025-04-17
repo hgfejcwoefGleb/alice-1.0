aa7bb5786a6d821af7fe08436913829d68be566c
hgfejcwoefGleb <114211284+hgfejcwoefGleb@users.noreply.github.com>
2025-04-17 11:52:04 +0300

    Add files via upload
    Получилось добавить и проверить регистрации по захаркоженному сценарию

--- interaction/scenes.py
+++ interaction/scenes.py
@@ -4,7 +4,7 @@ import inspect
 from request import Request
 from state import STATE_RESPONSE_KEY
 import intents
-from registration_ydb import *
+from ..ydb.registration_ydb import *
 
 
 class Scene(ABC):
@@ -27,16 +27,24 @@ class Scene(ABC):
 
     @abstractmethod
     def handle_local_intents(self, request: Request):
+        """Проверка локальных интентов,
+           по которым происходит переход к
+           другой сцене
+        """
         raise NotImplementedError
 
     @abstractmethod
     def handle_global_intents(self, request: Request):
+        """Проверка глобальных интентов,
+           по которым происходит переход к
+           другой сцене"""
         raise NotImplementedError
 
     def fallback(self, request: Request):
-        pass
+        return self.make_response(text="Извини, я не поняла твою просьбу. Переформулируй запрос или уточни, что я умею")
 
     def make_response(self, text, tts=None, card=None, state=None, buttons=None, user_state_update=None):
+        """Возврат ответа навыку"""
         response = {
             'text': text,
             'tts': tts if tts is not None else text
@@ -72,16 +80,35 @@ def is_registered(request: Request):
 def is_student(request: Request):  # перепишем с использованием интентов и сущностей
     return request['state']['user']['is_student'] == 'студент'
 
+#переписать без elif
 
 class Welcome(Scene):
     def handle_global_intents(self, request: Request):
-        print(is_registered(request))
-        if intents.GET_HELP in request.intents:
-            return GetHelp()
+        if intents.GET_HELP_IN_GENERAL in request.intents:
+            return GetHelpInGeneral()
         elif intents.REGISTRATE in request.intents or not is_registered(request):
             return Registration()
         elif intents.FIND_SCHEDULE in request.intents:
-            return Find_schedule()
+            return FindSchedule()
+        elif intents.CHANGE_USER_DATA in request.intents:
+            return ChangeUserData()
+        elif intents.CHANGE_SCHEDULE in request.intents:
+            return ChangeSchedule()
+        elif intents.ADD_LESSON in request.intents:
+            return AddLesson()
+        elif intents.GET_HELP_REG in request.intents:
+            return GetHelpReg()
+        elif intents.GET_HELP_FIND_SCH in request.intents:
+            return GetHelpFindSch()
+        elif intents.GET_HELP_CHANGE_DATA in request.intents:
+            return GetHelpChangeData()
+        elif intents.GET_HELP_CHANGE_SCH in request.intents:
+            return GetHelpChangeSch()
+        elif intents.GET_HELP_ADD_SCH in request.intents:
+            return GetHelpAddSch()
+        # только для теста потом убрать
+        else:
+            return IsStudent()
 
     def handle_local_intents(self, request: Request):
         pass
@@ -119,9 +146,9 @@ class InsertGroupData(Registration):
     def reply(self, request: Request, pool):
         # тут нужно подумать, как мы будет менять или получать id Группы, если она зарегана
         user_data = request["request"]['command']
-        print(user_data)  # здесь должно быть использование сущностей/интентов
+        # здесь должно быть использование сущностей/интентов
         text = ('Хорошо, теперь назови год своего поступления,'
-                'образовательную программу, факультет, формат обучения'
+                'образовательную программу, факультет, формат обучения '
                 'очный или нет и уровень образования')
         return self.make_response(text=text, user_state_update={'user_data': user_data})
 
@@ -132,37 +159,174 @@ class InsertGroupData(Registration):
 class InsertUserData(Registration):
     def reply(self, request: Request, pool):
         group_data = []
+        user_data = ""
         if is_student(request):
             user_data = request['state']['user']['user_data']
             # тут проверяем зарегана группа или нет
             group_data = request["request"]['command']
-            group = Group(*group_data.split())
+            group = Group(*list(group_data.split()))
             if is_group_reg(pool, group):
                 id_group = select_id_group(pool, group)
-            user_data = user_data.split()[:3]
-            user_data.append(id_group)
+                user_data = user_data.split()[:3]
+                user_data.append(id_group)
+            else:
+                user_data = list(user_data.split())
             # меняем id_group в сессии, если студент
         else:
             user_data = user_data.split()
-        registration_user(user_data, pool, is_student(request), group_data)
+        registration_user(user_data, pool, is_student(request), group_data.split())
         text = (
             'Очень приятно! Хочешь проверить свое расписание на сегодня, на какую-то другую дату или найти конкретный предмет?')
-        return self.make_response(text=text, user_state_update={'user_data': " ".join(user_data)})
+        return self.make_response(text=text, user_state_update={'user_data': " ".join(user_data), 'group_data': group_data})
+
+    def handle_local_intents(self, request: Request):
+        pass
+
+#возможно переписать фразы в соответствии с интентами
+#
+class GetHelpInGeneral(Welcome):
+    """"""
+    def reply(self, request: Request, pool):
+        text = ('Привет, я помогу узнать расписание твоих дисциплин '
+                'Например, если ты хочешь узнать расписание по философии, то'
+                'скажи: "Алиса когда у меня философия" или: Алиса, что у меня во вторник?.'
+                ' Я могу подсказать расписание на конкретную дату, день недели '
+                'или у нужного преподавателя. Если хочешь узнать, когда у тебя пара '
+                'с преподавателем, скажите, когда у меня пара с Владимиром Владимировым Владимиряном? '
+                'Главное, чтобы фамилия была правильной. Чтобы поменять данные, '
+                'скажи: хочу поменять данные. Чтобы добавить пару, скажи: '
+                '"Алиса, добавь пару". Чтобы получить справку по конкретной функции:'
+                '"Алиса как мне изменить данные/зарегистрироваться/найти расписание/изменить расписание/добавить расписание"')
+        return self.make_response(text=text)
+
+    def handle_local_intents(self, request: Request):
+        pass
+
+class ChangeUserData(Welcome):
+    def reply(self, request: Request, pool):
+        text = "Что ты хочешь поменять? Фамилию, имя, отчество номер группы или все в целом?"
+        return self.make_response(text=text)
+
+    def handle_local_intents(self, request: Request):
+        if intents.CHANGE_ALL_DATA in request.intents:
+            return ChangeAllData()
+        elif intents.CHANGE_NAME in request.intents:
+            return ChangeName()
+        elif intents.CHANGE_SURNAME in request.intents:
+            return ChangeSurname()
+        elif intents.CHANGE_FATHER_NAME in request.intents:
+            return ChangeFatherName()
+        elif intents.CHANGE_GROUP in request.intents:
+            return ChangeGroup()
+
+#перенести эту функцию в ydd
+def change_db_data(pool: ydb.QuerySessionPool, user_data_old: str, user_data_new):
+    """Функция по изменению данных пользователя в БД"""
+    student = Student(*user_data_old)
+    id_student = select_id_student(pool, student)
+    student = Student(*user_data_new)
+    pool.execute_with_retries(
+        f"""
+        DECLARE $id AS Int16; 
+        DECLARE $name AS Utf8;
+        DECLARE $surname AS Utf8;
+        DECLARE $father_name AS Utf8;
+        DECLARE $id_group AS Int16;
+        
+        UPSERT INTO Student(id, name, surname, father_name, id_group)
+        VALUES ($id, $name, $surname, $father_name, $id_group)
+        """,
+        {
+            '$id': id_student,
+            '$name': student.name,
+            '$surname': student.surname,
+            '$father_name': student.father_name,
+            '$id_group': student.id_group,
+        }
+    )
+
+class ChangeAllData(ChangeUserData):
+    def reply(self, request: Request, pool):
+        new_data = request['request']['command']
+
+        text = "Все данные успешно изменены!"
+        return self.make_response(text=text, user_state_update={'user_data': new_data})
 
     def handle_local_intents(self, request: Request):
         pass
 
 
-class GetHelp(Welcome):
+
+
+class ChangeName:
+    pass
+
+
+class ChangeSurname:
+    pass
+
+
+class ChangeFatherName:
+    pass
+
+
+class ChangeGroup:
     pass
 
 
 class FindSchedule(Welcome):
     pass
 
+class ChangeSchedule(Welcome):
+    pass
+
+
+class AddLesson(Welcome):
+    pass
+
+class GetHelpReg(GetHelpInGeneral):
+    pass
+
+
+class GetHelpFindSch(GetHelpInGeneral):
+    pass
+
+
+class GetHelpChangeData(GetHelpInGeneral):
+    pass
+
+
+class GetHelpChangeSch(GetHelpInGeneral):
+    pass
+
+
+class GetHelpAddSch(GetHelpInGeneral):
+    pass
+
 
 SCENES = {
-    scene.id(): scene for scene in [Welcome, Registration, IsStudent, InsertUserData, GetHelp, FindSchedule]
+    scene.id(): scene for scene in [
+        Welcome,
+        Registration,
+        IsStudent,
+        InsertGroupData,
+        InsertUserData,
+        GetHelpInGeneral,
+        ChangeUserData,
+        ChangeAllData,
+        ChangeName,
+        ChangeSurname,
+        ChangeFatherName,
+        ChangeGroup,
+        FindSchedule,
+        ChangeSchedule,
+        AddLesson,
+        GetHelpReg,
+        GetHelpFindSch,
+        GetHelpChangeData,
+        GetHelpChangeSch,
+        GetHelpAddSch
+    ]
 }
 
 DEFAULT_SCENE = Welcome
---
+++ ydb/registration_ydb.py
@@ -0,0 +1,323 @@
+from typing import Union
+from datetime import datetime
+
+import ydb
+
+
+class Lesson:
+    def __init__(self, name, type_l, building, auditorium, id_lecturer, time, is_weekly, is_upper, lesson_date):
+        # Общие атрибуты для всех уроков
+        self.name = name
+        self.type_l = type_l
+        self.building = building
+        self.auditorium = auditorium
+        self.id_lecturer = id_lecturer
+        self.time = time
+        self.is_weekly = is_weekly
+        self.is_upper = is_upper
+        self.lesson_date = lesson_date
+
+
+class PersonalLesson(Lesson):
+    def __init__(self, name, type_l, building, auditorium, id_lecturer, time, is_weekly, is_upper, lesson_date,
+                 id_student):
+        # Вызываем конструктор родительского класса для инициализации общих атрибутов
+        super().__init__(name, type_l, building, auditorium, id_lecturer, time, is_weekly, is_upper, lesson_date)
+        # Уникальный атрибут для PersonalLesson
+        self.id_student = id_student
+
+
+class GroupLesson(Lesson):
+    def __init__(self, name, type_l, building, auditorium, id_lecturer, time, is_weekly, is_upper, lesson_date,
+                 id_group):
+        # Вызываем конструктор родительского класса для инициализации общих атрибутов
+        super().__init__(name, type_l, building, auditorium, id_lecturer, time, is_weekly, is_upper, lesson_date)
+        # Уникальный атрибут для GroupLesson
+        self.id_group = id_group
+
+
+class Group:
+    def __init__(self, name, edu_year, edu_program, faculty, edu_format, edu_level):
+        # print(name, edu_year, edu_program, faculty, edu_format, edu_level)
+        self.name = name
+        self.edu_year = edu_year
+        self.edu_program = edu_program
+        self.faculty = faculty
+        self.edu_format = edu_format
+        self.edu_level = edu_level
+        # cur.execute('INSERT INTO "Group" (name, edu_year, edu_program, faculty, edu_format, edu_level) '
+        # 'VALUES (%s, %s, %s, %s, %s, %s);', list(vars(self).values())) #??????????
+        # cur.execute("""INSERT INTO "Group" (name, edu_year, edu_program, faculty, edu_format, edu_level)
+        # VALUES (2, 2, 2, 2, 2, 2);""")
+
+
+class Student:
+    def __init__(self, name, surname, father_name, id_group):
+        self.name = name
+        self.surname = surname
+        self.father_name = father_name
+        self.id_group = id_group
+
+
+class Lecturer:
+    def __init__(self, name, surname, father_name):
+        self.name = name
+        self.surname = surname
+        self.father_name = father_name
+
+
+class PersonalLessonStudent:
+    def __init__(self, id_student, id_personal_lesson):
+        self.id_student = id_student
+        self.id_personal_lesson = id_personal_lesson
+
+
+class GroupLessonGroup:
+    def __init__(self, id_group_lesson, id_group):
+        self.id_group_lesson = id_group_lesson
+        self.id_group = id_group
+
+
+class LecturerGroup:
+    def __int__(self, id_lecturer, id_group):
+        self.id_lecturer = id_lecturer
+        self.id_group = id_group
+
+def get_group_records(pool: ydb.QuerySessionPool, group: Group, is_exist_check: bool)-> list[ydb.convert._Row]:
+    attr = 'id'
+    if is_exist_check:
+        attr = '1'
+    return pool.execute_with_retries(
+        f"""
+        DECLARE $name AS Utf8;
+        DECLARE $edu_year AS Utf8;
+
+        SELECT {attr} FROM `Group`
+        WHERE name = $name AND edu_year = $edu_year;
+        """,
+        {
+            '$name': group.name,
+            '$edu_year': group.edu_year,
+        },
+    )[0].rows
+
+def select_id_group(pool: ydb.QuerySessionPool, group: Group)-> int:
+    records = sorted(get_group_records(pool, group, False), key=lambda x: x.id, reverse=True)
+    return records[0].id
+
+def is_group_reg(pool: ydb.QuerySessionPool, group: Group)-> bool:
+    records = get_group_records(pool, group, True)
+    return len(records) != 0
+
+def get_lesson_records(pool: ydb.QuerySessionPool, lesson: Union[GroupLesson, PersonalLesson], id_obj: int, is_exist_check: bool)-> list[ydb.convert._Row]:
+    attr = 'id'
+    if is_exist_check:
+        attr = '1'
+    unique_elem = ""
+    table = ""
+    if isinstance(lesson, GroupLesson):
+        unique_elem = 'id_group'
+        table = 'GroupLesson'
+    if isinstance(lesson, PersonalLesson):
+        unique_elem = 'id_student'
+        table = 'PersonalLesson'
+    return pool.execute_with_retries(
+        f"""
+            DECLARE ${unique_elem} AS Int64;
+            DECLARE $lesson_date AS Date;
+            DECLARE $time AS Utf8;
+
+            SELECT {attr} FROM `{table}`
+            WHERE {unique_elem} = ${unique_elem} AND lesson_date = $lesson_date AND time = $time;
+            """,
+        {
+            f'${unique_elem}': (id_obj, ydb.PrimitiveType.Int64),
+            '$lesson_date': (datetime.strptime(lesson.lesson_date, '%d.%m.%Y').date(), ydb.PrimitiveType.Date),
+            '$time': lesson.time
+        },
+    )[0].rows
+
+def select_id_lesson(pool: ydb.QuerySessionPool, lesson: Union[GroupLesson, PersonalLesson], id_obj: int)-> int:
+    records = sorted(get_lesson_records(pool, lesson, id_obj, False), key=lambda x: x.id, reverse=True)
+    return records[0].id
+
+def is_lesson_reg(pool: ydb.QuerySessionPool, lesson: Union[GroupLesson, PersonalLesson], id_obj: int)-> bool:
+    records = get_lesson_records(pool, lesson, id_obj, True)
+    return len(records) != 0
+
+def get_student_records(pool: ydb.QuerySessionPool, student: Student, is_exist_check:bool)-> list[ydb.convert._Row]:
+    attr = 'id'
+    if is_exist_check:
+        attr = '1'
+    return pool.execute_with_retries(
+        f"""
+        DECLARE $name AS Utf8;
+        DECLARE $surname AS Utf8;
+        DECLARE $father_name AS Utf8;
+        DECLARE $id_group AS Int64;
+
+        SELECT {attr} FROM `Student`
+        WHERE name = $name AND surname = $surname AND father_name = $father_name AND id_group = $id_group;
+        """,
+        {
+            '$name': student.name,
+            '$surname': student.surname,
+            '$father_name': student.father_name,
+            '$id_group': (student.id_group, ydb.PrimitiveType.Int64),
+        },
+    )[0].rows
+
+def select_id_student(pool: ydb.QuerySessionPool, student: Student)-> int:
+    records = sorted(get_student_records(pool, student, False), key=lambda x: x.id, reverse=True)
+    return records[0].id
+
+def is_student_reg(pool: ydb.QuerySessionPool, student: Student)-> bool:
+    records = get_student_records(pool, student, True)
+    return len(records) != 0
+
+def get_lecturer_records(pool: ydb.QuerySessionPool, lecturer: Lecturer, is_exist_check: bool)-> list[ydb.convert._Row]:
+    attr = 'id'
+    if is_exist_check:
+        attr = '1'
+    return pool.execute_with_retries(
+        f"""
+        DECLARE $name AS Utf8;
+        DECLARE $surname AS Utf8;
+        DECLARE $father_name AS Utf8;
+
+        SELECT {attr} FROM `Lecturer`
+        WHERE name = $name AND surname = $surname AND father_name = $father_name;
+        """,
+        {
+            '$name': lecturer.name,
+            '$surname': lecturer.surname,
+            '$father_name': lecturer.father_name,
+        },
+    )[0].rows
+
+def select_id_lecturer(pool: ydb.QuerySessionPool, lecturer: Lecturer)-> int:
+    records = sorted(get_lecturer_records(pool, lecturer, False), key=lambda x: x.id, reverse=True)
+    return records[0].id
+
+def is_lecturer_reg(pool: ydb.QuerySessionPool, lecturer: Lecturer)-> bool:
+    records = get_lecturer_records(pool, lecturer, True)
+    return len(records) != 0
+
+
+def connect_lecturer_with_group(pool: ydb.QuerySessionPool, id_group: int, id_lecturer: int):
+    res = (pool.execute_with_retries
+        (
+        """
+        DECLARE $id_lecturer AS Int64;
+        DECLARE $id_group AS Int64;
+
+        SELECT 1 FROM `LecturerGroup`
+        WHERE id_lecturer = $id_lecturer AND id_group = $id_group;
+        """,
+        {
+            "$id_lecturer": (id_lecturer, ydb.PrimitiveType.Int64),
+            "$id_group": (id_group, ydb.PrimitiveType.Int64)
+        },
+    ))
+    if len(res[0].rows) == 0:
+        pool.execute_with_retries(
+            """
+            DECLARE $id_lecturer AS Int64;
+            DECLARE $id_group AS Int64;
+
+            INSERT INTO LecturerGroup(id_lecturer, id_group)
+            VALUES ($id_lecturer, $id_group);
+            """,
+            {
+                "$id_lecturer": (id_lecturer, ydb.PrimitiveType.Int64),
+                "$id_group": (id_group, ydb.PrimitiveType.Int64)
+            },
+        )
+
+
+
+def registration_user(user_data: list, pool: ydb.QuerySessionPool, is_student=True, group_data: list= None):
+    """Регистрирует пользователя и группу в БД."""
+    if is_student:
+        group = Group(*group_data)
+        student = Student(*user_data)
+        # Проверяем, существует ли группа
+        if not is_group_reg(pool, group):
+            # Если группы нет, добавляем её
+            reg_group(pool, group)
+            #тут потенциально о
+            student.id_group = select_id_group(pool, group)  # Получаем id новой группы
+        if not is_student_reg(pool, student):
+            reg_student(pool, student, student.id_group)
+        else:
+            return -1  #пользователь зарегистрирован
+    else:
+        lecturer = Lecturer(*user_data)
+        if not is_lecturer_reg(pool, lecturer):
+            reg_lecturer(pool, lecturer)
+        else:
+            return -1 #пользователь зарегистрирован
+
+
+def reg_group(pool: ydb.QuerySessionPool, group: Group) -> None:
+    pool.execute_with_retries(
+        """
+        DECLARE $name AS Utf8;
+        DECLARE $edu_year AS Utf8;
+        DECLARE $edu_program AS Utf8;
+        DECLARE $faculty AS Utf8;
+        DECLARE $edu_format AS Utf8;
+        DECLARE $edu_level AS Utf8;
+
+        INSERT INTO `Group` (name, edu_year, edu_program, faculty, edu_format, edu_level)
+        VALUES ($name, $edu_year, $edu_program, $faculty, $edu_format, $edu_level);
+        """,
+        {
+            '$name': group.name,
+            '$edu_year': group.edu_year,
+            '$edu_program': group.edu_program,
+            '$faculty': group.faculty,
+            '$edu_format': group.edu_format,
+            '$edu_level': group.edu_level
+        },
+    )
+
+
+def reg_student(pool: ydb.QuerySessionPool, student: Student, id_group: int) -> None:
+    pool.execute_with_retries(
+        """
+        DECLARE $name AS Utf8;
+        DECLARE $surname AS Utf8;
+        DECLARE $father_name AS Utf8;
+        DECLARE $id_group AS Int16;
+
+        INSERT INTO Student (name, surname, father_name, id_group)
+        VALUES ($name, $surname, $father_name, $id_group);
+        """,
+        {
+            '$name': student.name,
+            '$surname': student.surname,
+            '$father_name': student.father_name,
+            '$id_group': (id_group, ydb.PrimitiveType.Int16),
+
+        },
+    )
+
+
+def reg_lecturer(pool: ydb.QuerySessionPool, lecturer: Lecturer) -> None:
+    pool.execute_with_retries(
+        """
+        DECLARE $name AS Utf8;
+        DECLARE $surname AS Utf8;
+        DECLARE $father_name AS Utf8;
+
+        INSERT INTO Lecturer (name, surname, father_name)
+        VALUES ($name, $surname, $father_name);
+        """,
+        {
+            '$name': lecturer.name,
+            '$surname': lecturer.surname,
+            '$father_name': lecturer.father_name,
+        },
+    )
+
